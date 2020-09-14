3dcdd3dbc2bda3225f241d8eef802265896ad1e1
wbadmin <>
2020-09-14 15:23:26 +0300

    {/src/main/java/com/workshops_diy/coolstore/ShoppingCartItem.java}

--- src/main/java/com/workshops_diy/coolstore/ShoppingCartItem.java
+++ src/main/java/com/workshops_diy/coolstore/ShoppingCartItem.java
@@ -7,29 +7,103 @@ package com.workshops_diy.coolstore;
 @javax.persistence.Entity
 public class ShoppingCartItem implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "SHOPPINGCARTITEM_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "SHOPPINGCARTITEM_ID_GENERATOR", sequenceName = "SHOPPINGCARTITEM_ID_SEQ")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "SHOPPINGCARTITEM_ID_GENERATOR")
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(name = "SHOPPINGCARTITEM_ID_GENERATOR", sequenceName = "SHOPPINGCARTITEM_ID_SEQ")
+	private java.lang.Long id;
 
-    public ShoppingCartItem() {
-    }
-    
-    public ShoppingCartItem(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Item ID")
+	private java.lang.String itemId;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Name")
+	private java.lang.String name;
 
+	@org.kie.api.definition.type.Label(value = "Price")
+	private java.lang.Double price;
 
+	@org.kie.api.definition.type.Label(value = "Promotional Savings")
+	private java.lang.String promoSavings;
 
+	@org.kie.api.definition.type.Label(value = "Quantity")
+	private java.lang.Integer quantity;
+
+	@javax.persistence.ManyToOne(cascade = {javax.persistence.CascadeType.ALL}, fetch = javax.persistence.FetchType.EAGER)
+	@org.kie.api.definition.type.Label(value = "Shopping Cart")
+	private com.workshops_diy.coolstore.ShoppingCart shoppingCart;
+
+	public ShoppingCartItem() {
+	}
+
+	public java.lang.Long getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
+
+	public java.lang.String getItemId() {
+		return this.itemId;
+	}
+
+	public void setItemId(java.lang.String itemId) {
+		this.itemId = itemId;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.Double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(java.lang.Double price) {
+		this.price = price;
+	}
+
+	public java.lang.String getPromoSavings() {
+		return this.promoSavings;
+	}
+
+	public void setPromoSavings(java.lang.String promoSavings) {
+		this.promoSavings = promoSavings;
+	}
+
+	public java.lang.Integer getQuantity() {
+		return this.quantity;
+	}
+
+	public void setQuantity(java.lang.Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public com.workshops_diy.coolstore.ShoppingCart getShoppingCart() {
+		return this.shoppingCart;
+	}
+
+	public void setShoppingCart(
+			com.workshops_diy.coolstore.ShoppingCart shoppingCart) {
+		this.shoppingCart = shoppingCart;
+	}
+
+	public ShoppingCartItem(java.lang.Long id, java.lang.String itemId,
+			java.lang.String name, java.lang.Double price,
+			java.lang.String promoSavings, java.lang.Integer quantity,
+			com.workshops_diy.coolstore.ShoppingCart shoppingCart) {
+		this.id = id;
+		this.itemId = itemId;
+		this.name = name;
+		this.price = price;
+		this.promoSavings = promoSavings;
+		this.quantity = quantity;
+		this.shoppingCart = shoppingCart;
+	}
 
 }
